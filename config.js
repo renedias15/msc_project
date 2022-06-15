e75c9665d314d2c146190f219b8a328267845ebf
Rene <71095615+us69er@users.noreply.github.com>
2022-06-15 23:20:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb+srv://rene:1234@cluster0.d5sorgc.mongodb.net/?retryWrites=true&w=majority'
+    url: "mongodb+srv://rene:1234@cluster0.d5sorgc.mongodb.net/?retryWrites=true&w=majority"
 }
