ed70738835eb3208bfe742ec0c0e065aac0aef8a
Rene <71095615+renedias15@users.noreply.github.com>
2023-06-23 12:13:17 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: 'mongodb+srv://rene:1234@cluster0.d5sorgc.mongodb.net/?retryWrites=true&w=majority' // taking time like
+    // url: 'mongodb://localhost:27017/project'
+}
