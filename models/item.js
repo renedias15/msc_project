71e033fa132b8d1456b2b57434904e11a472a038
Rene <71095615+renedias15@users.noreply.github.com>
2023-06-24 11:42:40 +0530

    Update item.js

--- models/item.js
+++ models/item.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema
 const itemSchema = new Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
-    description:{type: String, required: true},
+    desc:{type: String, required: true},
     price: { type: Number, required: true },
     size: { type: String, required: true },
 
